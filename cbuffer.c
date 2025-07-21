9c3f3d72f8ca4d6ec81770be65c147f02d61cb9d
Dai Tran <tranquocdai2003@gmail.com>
2025-07-21 17:35:28 +0700

    feat: Add cbuffer.c

---
+++ cbuffer.c
@@ -0,0 +1,106 @@
+/**
+ * @file       <file_name>.c
+ * @copyright  Copyright (C) 2019 ITRVN. All rights reserved.
+ * @license    This project is released under the Fiot License.
+ * @version    major.minor.patch
+ * @date       2025-07-211
+ * @author     Dai Tran
+ * @author     Hai Tran
+ * @author     Thanh Le  
+ * @author     Han Pham
+ *           
+ * @brief      <A brief description of the content of the file>
+ *             
+ * @note          
+ * @example    example_file_1.c
+ *             Example_1 description
+ * @example    example_file_2.c
+ *             Example_2 description
+ */
+
+/* Includes ----------------------------------------------------------- */
+#include <stdint.h>
+#include <string.h>
+
+#include "cbuffer.h"
+/* Private defines ---------------------------------------------------- */
+
+
+/* Private enumerate/structure ---------------------------------------- */
+/**
+ * @brief <enum descriptiton>
+ */
+
+
+/**
+ * @brief <structure descriptiton>
+ */
+
+
+
+
+/* Private macros ----------------------------------------------------- */
+/**
+ * @brief  <macro description>
+ *
+ * @param[in]     <param_name>  <param_despcription>
+ * @param[out]    <param_name>  <param_despcription>
+ * @param[inout]  <param_name>  <param_despcription>
+ *
+ * @attention  <API attention note>
+ *
+ * @return  
+ *  - 0: Success
+ *  - 1: Error
+ */
+
+     
+//#define INITIALIZE_WRITER_READER_OVERFLOW
+/* Public variables --------------------------------------------------- */
+
+
+/* Private variables -------------------------------------------------- */
+
+/* Private function prototypes ---------------------------------------- */
+/**
+ * @brief  <function description>
+ *
+ * @param[in]     <param_name>  <param_despcription>
+ * @param[out]    <param_name>  <param_despcription>
+ * @param[inout]  <param_name>  <param_despcription>
+ *
+ * @attention  <API attention note>
+ *
+ * @return  
+ *  - 0: Success
+ *  - 1: Error
+ */
+
+/* Function definitions ----------------------------------------------- */
+void cb_init(cbuffer_t *cb, void *buf, uint32_t size)
+{ 
+}
+
+void cb_clear(cbuffer_t *cb)
+{
+}
+
+uint32_t cb_read(cbuffer_t *cb, void *buf, uint32_t nbytes)
+{ 
+}
+
+uint32_t cb_write(cbuffer_t *cb, void *buf, uint32_t nbytes)
+{
+}
+
+uint32_t cb_data_count(cbuffer_t *cb)
+{
+}
+
+uint32_t cb_space_count(cbuffer_t *cb)
+{
+}
+/* Private definitions ----------------------------------------------- */
+
+
+/* End of file -------------------------------------------------------- */
